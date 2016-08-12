352dec5e66933e100987bfa9e789ae3bfd913c3b
bacancy <info@bacancytechnology.com>
2016-08-12 18:05:59 +0530

    Hima change

--- TestWappImagepicker/ViewController.m
+++ TestWappImagepicker/ViewController.m
@@ -37,7 +37,7 @@
 {
     [super viewWillAppear:YES];
     
-    NSLog(@"AppDelegate Test Array == %@",[self appDelegate].testArray);
+    NSLog(@"AppDelegate Test HIma == %@",[self appDelegate].testArray);
 }
 
 -(IBAction) imagepickerData:(id)sender
